3476be3f2aa6099fb75c2b7410773615f1f22678
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-11-19 19:02:05 -0800

    Updates

---
+++ precache-manifest.c417fa695e93f4a4fb11e75f6ef9681e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65c0d117e1c9513048009782f01d471",
+    "url": "/FindYourFavPic/index.html"
+  },
+  {
+    "revision": "737bcb92d45ccded8b25",
+    "url": "/FindYourFavPic/static/css/2.969b5337.chunk.css"
+  },
+  {
+    "revision": "737bcb92d45ccded8b25",
+    "url": "/FindYourFavPic/static/js/2.62d6f1eb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/FindYourFavPic/static/js/2.62d6f1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac090431d61339679440",
+    "url": "/FindYourFavPic/static/js/main.e45203aa.chunk.js"
+  },
+  {
+    "revision": "5a6ea85165c8c2210246",
+    "url": "/FindYourFavPic/static/js/runtime-main.20bab229.js"
+  },
+  {
+    "revision": "e14d611ea30fa431e9d7d3855036d855",
+    "url": "/FindYourFavPic/static/media/logo.e14d611e.svg"
+  }
+]);
